ca1f063a9c72a8b833074490dfd4f1aa12687752
yumiinLEE <leeum1111@naver.com>
2025-02-19 10:27:41 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 151 ms, Memory: 48,388 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/README.md
@@ -0,0 +1,15 @@
+# [D4] [S/W 문제해결 기본] 7일차 - 미로1 - 1226 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14vXUqAGMCFAYD) 
+
+### 성능 요약
+
+메모리: 48,388 KB, 시간: 151 ms, 코드길이: 550 Bytes
+
+### 제출 일자
+
+2024-11-15 23:31
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int[][] matrix;
+	static Queue<int[]> q;
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	static boolean[][] visited;
+	
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int t = 1; t <= 10; t++) {
+        	int T = Integer.parseInt(br.readLine());
+        	
+        	matrix = new int[16][16];
+        	
+        	for (int i = 0; i < 16; i++) {
+				String line[] = br.readLine().split("");
+				for (int j = 0; j < 16; j++) {
+					matrix[i][j] = Integer.parseInt(line[j]);
+				}
+        	}
+        	q = new LinkedList<>();
+        	visited = new boolean[16][16];
+							
+        	System.out.println("#" + t + " " + bfs(2, 2));
+        }
+	}
+
+	public static int bfs(int x, int y) {
+		q.add(new int[] {x, y});
+		
+		while (!q.isEmpty()) {
+			int[] currnet = q.poll();
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = currnet[0] + dx[i];
+				int ny = currnet[1] + dy[i];
+				
+				if (matrix[nx][ny]==3) {
+					return 1;
+				}
+				
+				if (nx>=0 && nx<16 && ny>=0 && ny<16 && matrix[nx][ny]==0 && !visited[nx][ny]) {
+					q.add(new int[] {nx, ny});
+					visited[nx][ny] = true;
+				}
+			}
+		}
+		return 0;
+	}
+}
