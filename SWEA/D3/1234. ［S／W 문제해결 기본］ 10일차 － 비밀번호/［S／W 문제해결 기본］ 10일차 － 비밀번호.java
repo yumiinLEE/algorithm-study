19878fb080dfd7c8964426cc29bbec9fef614d2e
yumiinLEE <leeum1111@naver.com>
2025-02-20 10:50:01 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 89 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
@@ -2,34 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+   
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
+        
         for (int t = 1; t <= 10; t++) {
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
-            String line = st.nextToken();
+            String pass = st.nextToken();
             
-            // Deque 사용 (스택처럼 처리)
-            Deque<Character> stack = new ArrayDeque<>();
-
-            for (char num : line.toCharArray()) {
-                // 스택이 비어 있지 않고, 현재 값과 스택의 마지막 값이 같으면 pop
-                if (!stack.isEmpty() && stack.peekLast() == num) {
-                    stack.pollLast();  // 가장 마지막 값 제거 (pop)
-                } else {
-                    stack.offerLast(num);  // 새로운 값은 스택에 추가 (push)
-                }
+            Stack<Character> stack = new Stack<>();
+            
+            for (int i = 0; i < N; i++) {
+            	char current = pass.charAt(i);
+    			if (!stack.isEmpty() && stack.peek().equals(current)) {
+    				stack.pop();
+    			}
+    			else {
+    				stack.add(current);
+    			}
+    		}
+            
+            StringBuilder sb = new StringBuilder();
+            
+            while (!stack.isEmpty()) {
+            	sb.insert(0, stack.pop());
             }
-
-            // 결과 출력: 스택에 남은 문자들 출력
-            StringBuilder result = new StringBuilder();
-            for (char c : stack) {
-                result.append(c);
-            }
-
-            System.out.println("#" + t + " " + result.toString());
-        }
+            System.out.println("#" + t + " " + sb.toString());
+		}
     }
 }
