6ee3c7dec2b219dcba048cad7b188a98f3fa8a66
yumiinLEE <leeum1111@naver.com>
2025-02-21 09:57:50 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 151 ms, Memory: 48,388 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,52 +1,55 @@
 import java.io.*;
 import java.util.*;
- 
+
 public class Solution {
-    static int[][] matrix;
-    static Queue<int[]> q;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-     
-    public static void main(String args[]) throws Exception{
+	static int[][] matrix;
+	static int[] dx = {0, 0, 1, -1};
+	static int[] dy = {1, -1, 0, 0};
+	static Queue<int[]> q;
+	
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
+        
         for (int t = 1; t <= 10; t++) {
-            int T = Integer.parseInt(br.readLine());
-             
-            matrix = new int[16][16];
-             
-            for (int i = 0; i < 16; i++) {
-                String line[] = br.readLine().split("");
-                for (int j = 0; j < 16; j++) {
-                    matrix[i][j] = Integer.parseInt(line[j]);
-                }
-            }
-            q = new LinkedList<>();
-                             
-            System.out.println("#" + t + " " + bfs(2, 2));
+        	int T = Integer.parseInt(br.readLine());
+        	
+        	q = new LinkedList<>();
+        	
+        	matrix = new int[16][16];
+        	for (int i = 0; i < 16; i++) {
+				String line[] = br.readLine().split("");
+				for (int j = 0; j < 16; j++) {
+					matrix[i][j] = Integer.parseInt(line[j]);
+					
+					if (matrix[i][j] == 2) {
+						q.add(new int[] {i, j});
+					}
+				}
+			}
+        	System.out.println("#" + t + " " + bfs());
         }
-    }
- 
-    public static int bfs(int x, int y) {
-        q.add(new int[] {x, y});
-         
-        while (!q.isEmpty()) {
-            int[] currnet = q.poll();
-             
-            for (int i = 0; i < 4; i++) {
-                int nx = currnet[0] + dx[i];
-                int ny = currnet[1] + dy[i];
-                 
-                if (matrix[nx][ny]==3) {
-                    return 1;
-                }
-                 
-                if (nx>=0 && nx<16 && ny>=0 && ny<16 && matrix[nx][ny]==0) {
-                    q.add(new int[] {nx, ny});
-                    matrix[nx][ny] = 1;
-                }
-            }
-        }
-        return 0;
-    }
+	}
+
+	public static int bfs() {
+		
+		while(!q.isEmpty()) {
+			int[] current = q.poll();
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = current[0] + dx[i];
+				int ny = current[1] + dy[i];
+				
+				if (nx>=0 && nx<16 && ny>=0 && ny<16) {
+					if (matrix[nx][ny] == 3) {
+						return 1;
+					}
+					if (matrix[nx][ny] == 0) {
+						q.add(new int[] {nx, ny});
+						matrix[nx][ny] = 1;
+					}
+				}
+			}
+		}
+		return 0;
+	}
 }
