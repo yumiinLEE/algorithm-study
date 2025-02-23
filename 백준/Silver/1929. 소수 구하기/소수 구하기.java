eb4e8c8d54e93a7a3a0a70d1960690eb8a31b171
yumiinLEE <leeum1111@naver.com>
2025-02-23 16:39:02 +0900

    [Silver III] Title: 소수 구하기, Time: 268 ms, Memory: 21660 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,31 +1,42 @@
-
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static boolean[] prime;
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		int M=in.nextInt();
-		int N=in.nextInt();
-		
-		prime = new boolean[N+1];
-		get_prime();
-		
-		for(int i=M; i<=N; i++) {
-			if (!prime[i]) System.out.println(i);
-		}
-	}
-	
-	public static void get_prime() {
-		prime[0]=prime[1]=true;
-		
-		for(int i=2; i<=Math.sqrt(prime.length); i++) {
-			if(prime[i]) continue;
-			for(int j=i*i; j<prime.length; j+=i) {
-				prime[j]=true;
-			}
-		}
-	}
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+   
+        // N까지의 수에 대한 소수 여부를 기록할 배열
+        boolean[] isPrime = new boolean[N + 1];
+        
+        // 초기값은 모두 소수라고 가정
+        Arrays.fill(isPrime, true);
+        
+        // 0과 1은 소수가 아니므로 false로 설정
+        isPrime[0] = isPrime[1] = false;
+        
+        // 에라토스테네스의 체 알고리즘으로 소수를 구하기
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                // i가 소수라면 i의 배수들을 모두 소수가 아니라고 설정
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        // M부터 N까지 소수를 출력
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+        
+        System.out.print(sb);
+    }
 }
