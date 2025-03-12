8b285814a4dcc70b6fe50495b7cc265ea37804ae
yumiinLEE <leeum1111@naver.com>
2025-03-12 17:33:34 +0900

    [Silver IV] Title: 문자열 집합, Time: 360 ms, Memory: 30816 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,33 +1,30 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] line = br.readLine().split(" ");
-        
-        int N = Integer.parseInt(line[0]);
-        int M = Integer.parseInt(line[1]);
-        
-     // HashSet을 사용하여 집합 S에 포함된 문자열 저장
-        HashSet<String> set = new HashSet<>();
-        for (int i = 0; i < N; i++) {
-        	String s = br.readLine();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		Set<String> set = new HashSet<>();
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
 			set.add(s);
 		}
-        
-        //집합 S에 포함된 문자열 개수를 계산
-        int cnt = 0;
-        for (int i = 0; i < M; i++) {
-        	String s = br.readLine();
-        	if(set.contains(s)) {
-        		cnt++;
-        	}
+		
+		int cnt = 0;
+		for (int i = 0; i < M; i++) {
+			String test = br.readLine();
+			
+			if (set.contains(test)) {
+				cnt++;
+			}
 		}
-    System.out.print(cnt);
-    }
+		
+		System.out.println(cnt);
+	}
 }
