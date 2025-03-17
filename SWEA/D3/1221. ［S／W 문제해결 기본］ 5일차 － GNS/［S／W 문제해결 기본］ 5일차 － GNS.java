3ccf4cc8b85e79a4f8c1eab217ecb9013a19ab48
yumiinLEE <leeum1111@naver.com>
2025-03-17 13:41:38 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 152 ms, Memory: 41,208 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.java
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.java
@@ -1,47 +1,40 @@
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+	static String[] num = {"ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"};
 
-        // 숫자 단어를 배열로 저장
-        String[] numberWords = {"ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"};
-        
-        // 숫자 단어의 개수를 세기위한 배열
-        int[] count = new int[10];
-
-        // 테스트 케이스
-        int T = Integer.parseInt(br.readLine());
-
-        for (int t = 1; t <= T; t++) {
-            br.readLine(); // 읽고 버림
-            String[] words = br.readLine().split(" "); // 단어를 배열로 저장
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			st.nextToken();
+			int N = Integer.parseInt(st.nextToken());
+			
+			int[] cnt = new int[10];
+			
+			st = new StringTokenizer(br.readLine());
+			
+			for (int i = 0; i < N; i++) {
+				String word = st.nextToken();
+				for (int j = 0; j < 10; j++) {
+					if (word.equals(num[j])) {
+						cnt[j]++;
+						break;
+					}
+				}
+			}
             
-            // 개수 세기
-            for (String word : words) {
-                for (int i = 0; i < 10; i++) {
-                    if (word.equals(numberWords[i])) {
-                        count[i]++;
-                        break;
-                    }
-                }
-            }
-
-            // 출력 만들기
-            sb.append("#").append(t).append(" ");
-            for (int i = 0; i < 10; i++) {
-                while (count[i]-- > 0) {
-                    sb.append(numberWords[i]).append(" ");
-                }
-            }
-            sb.append("\n");
-
-            // 개수 배열 초기화
-            count = new int[10];
-        }
-
-        // 한 번에 출력
-        System.out.print(sb.toString());
-    }
+			System.out.print("#" + t + "\n");
+			for (int i = 0; i < 10; i++) {
+				for (int j = 0; j < cnt[i]; j++) {
+					System.out.print(num[i] + " ");
+				}
+			}
+		}
+	}
 }
