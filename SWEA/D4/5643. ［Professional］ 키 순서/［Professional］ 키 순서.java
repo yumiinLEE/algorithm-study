1df7c0932fb53919e9aaec91bbe3a83b89f4e06e
yumiinLEE <leeum1111@naver.com>
2025-05-22 23:46:05 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,596 ms, Memory: 101,244 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,74 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int N;
-	static int M;
-    static boolean[] visited;
-    static LinkedList<Integer>[] lst_child;
-    static LinkedList<Integer>[] lst_parnet;
-    static Queue<Integer> q;
-    
-    public static void main(String args[]) throws Exception{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int T = Integer.parseInt(br.readLine());
+        
+        int T = Integer.parseInt(br.readLine());  
 
         for (int t = 1; t <= T; t++) {
-            N = Integer.parseInt(br.readLine());
-            M = Integer.parseInt(br.readLine());
+            int N = Integer.parseInt(br.readLine());  // 학생 수
+            int M = Integer.parseInt(br.readLine());  // 비교 횟수
 
-            lst_child = new LinkedList[N+1];
-            lst_parnet = new LinkedList[N+1];
-            for (int i = 1; i <= N; i++) {
-            	lst_child[i] = new LinkedList<>();
-                lst_parnet[i] = new LinkedList<>();
-            }
+            // 학생 간 키 비교 결과를 저장할 그래프
+            boolean[][] graph = new boolean[N + 1][N + 1];
 
+            // M개의 비교 정보 입력받기
             for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                lst_child[b].add(a);
-                lst_parnet[a].add(b);
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());  // a번 학생
+                int b = Integer.parseInt(st.nextToken());  // b번 학생
+                graph[a][b] = true;  // a < b 이므로 a -> b
             }
-            
-            int cnt = 0;
-            
-            q = new LinkedList<>();
+
+            // 플로이드-워셜 알고리즘으로 간접적인 키 비교 관계 파악
+            for (int k = 1; k <= N; k++) {          // 경유 노드
+                for (int i = 1; i <= N; i++) {       // 출발 노드
+                    for (int j = 1; j <= N; j++) {   // 도착 노드
+                        // i < k 이고 k < j 이면 i < j 임을 유추할 수 있음
+                        if (graph[i][k] && graph[k][j]) {
+                            graph[i][j] = true;
+                        }
+                    }
+                }
+            }
+
+            int answer = 0;  // 자신의 정확한 순위를 알 수 있는 학생 수
+
+            // 모든 학생에 대해 검사
             for (int i = 1; i <= N; i++) {
-            	visited = new boolean[N+1];
-            	
-				bfs(i, lst_child);
-				bfs(i, lst_parnet);
-				
-				for (int j = 1; j <= N; j++) {
-					if (!visited[j]) {
-						break;
-					}
-					if (j == N) {
-						cnt++;
-					}
-				}
-			}
-            System.out.println("#" + t + " " + cnt);
+                int smaller = 0;  // 자신보다 키가 작은 학생 수
+                int taller = 0;   // 자신보다 키가 큰 학생 수
+
+                for (int j = 1; j <= N; j++) {
+                    if (graph[i][j]) taller++;      // i < j 이면 i보다 큰 학생
+                    if (graph[j][i]) smaller++;     // j < i 이면 i보다 작은 학생
+                }
+
+                // 자신보다 큰 학생 수 + 작은 학생 수 = N-1 이면 정확한 순위 파악 가능
+                if (taller + smaller == N - 1) {
+                    answer++;
+                }
+            }
+
+            System.out.println("#" + t + " " + answer);
         }
     }
-
-	public static void bfs(int student, LinkedList<Integer>[] lst) {
-		q.add(student);
-		visited[student] = true;
-		
-		while(!q.isEmpty()) {
-			int current = q.poll();
-			
-			for (int s : lst[current]) {
-				if (!visited[s]) {
-					q.add(s);
-					visited[s] = true;
-				}
-			}
-		}
-	}
 }
